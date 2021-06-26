9a7e2ef4d1099a950eac45a17071e8c492c868e0
likithaanusha <84706024+likithaanusha@users.noreply.github.com>
2021-06-26 23:46:28 +0530

    Add files via upload

---
+++ Oprnt7.java
@@ -0,0 +1,10 @@
+package pack1;
+public class Oprnt7 {
+      public static void main(String[] args) {
+		 int a = 6;
+		 int b = 7;
+		 System.out.println(a | b);
+		 System.out.println(a & b);
+		 System.out.println(a ^ b);
+	}
+}
