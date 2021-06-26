f719f7a5886b87146167e012074c001f686d2ac4
likithaanusha <84706024+likithaanusha@users.noreply.github.com>
2021-06-26 23:47:50 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,13 @@
+package cond;
+import java.util.*;
+public class If_Exp1 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter Age: ");
+		int a = sc.nextInt();
+		if(a > 18) 
+		{	System.out.println("Age is above 18"); }
+	}
+}
