e0bfdeb1959e57ac642b62a184548aa43bc16b4c
likithaanusha <84706024+likithaanusha@users.noreply.github.com>
2021-06-26 23:47:11 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+package pack1;
+import java.util.*;
+public class Sum 
+{
+	public static void main(String[] args) 
+	{
+	Scanner sc= new Scanner(System.in);
+    System.out.println("Enter 2 numbers: ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = a+b;
+    System.out.println("sum = " +c);
+	}
+}
