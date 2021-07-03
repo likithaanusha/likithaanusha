5f6adae272c5c5972dd8baebd9d8c0694c8725bc
likithaanusha <84706024+likithaanusha@users.noreply.github.com>
2021-07-03 22:19:34 +0530

    Add files via upload

---
+++ Sntnce.java
@@ -0,0 +1,10 @@
+package pack1;
+import java.util.*;
+public class Sntnce {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter sentence: ");
+		String s = sc.nextLine() ;
+		System.out.println("Sentence = " + s);
+		}
+}
